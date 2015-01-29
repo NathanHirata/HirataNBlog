c78c8a0f27c79cf3b6a5697f85ab2de4e91380aa
Nathan Hirata <nathanhirata@yahoo.com>
2015-01-29 15:02:16 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>NathanBlog</title>
+    </head>
+    <body>
+        <?php
+        echo "web server is running locally";
+        ?>
+    </body>
+</html>
