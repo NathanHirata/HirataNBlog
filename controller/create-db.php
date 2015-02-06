37a13f337d0f27cdcdb5e0927832d03a4fa4e645
Nathan Hirata <nathanhirata@yahoo.com>
2015-02-06 15:01:32 -0800

    5 commit

--- controller/create-db.php
+++ controller/create-db.php
@@ -10,8 +10,22 @@ require_once (__DIR__ . "/../model/database.php");
     $exists = $connection->select_db($database);
     
     if(!$exists) {
-        echo "Database does not exists";
+        $query = $connection->query("CREATE DATABASE $database");
+        
+        if($query) {
+            echo "Succesfully created database: " . $database;
+        }
+    }
+    else {
+        echo "Database already exists";
     }
        
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int(11) NOT NULL AUTO_INCREMENT,"
+            . "title varchar(255) NOT NULL"
+            . "post text NOT NULL,"
+            . "PRIMARY KEY (id)) ");
+    
+    if()
     
     $connection->close();
