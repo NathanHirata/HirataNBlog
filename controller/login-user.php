c9360ee91edfc30a16f20fbc3ea447bad0acde08
Nathan Hirata <nathanhirata@yahoo.com>
2015-02-25 15:02:15 -0800

    13?

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,3 +1,28 @@
 <?php
-echo "successfully logged in user";
+require_once (__DIR__ . "/../model/config.php");
+
+$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+echo $username;
+
+
+$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+
+if($query->num_rows == 1) {
+    $row = $query->fetch_array();
+    
+    if($row["password"] === crypt($password, $row["salt"])) {
+        $_SESSION["authenticated"] = true;
+        echo "<p> login succsseful </p>";
+    }
+    else {
+        echo "<p><Invalid Username And Password1/p>";
+    }
+}
+else {
+    echo "<p>invalid username or password2</p>";
+}
+
+
 
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+    require_once(__DIR__ . "/controller/login-verify.php");
     require_once(__DIR__ . "/view/header.php");
+    if(authenticatUser()) {
     require_once(__DIR__ . "/view/navagation.php");
+    }
     require_once(__DIR__ . "/controller/create-db.php");
     require_once(__DIR__ . "/view/footer.php");
     require_once(__DIR__ . "/controller/read-posts.php");
